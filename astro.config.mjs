3373dad8650ff87e3317780d3199e4b84e74272f
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2024-06-15 15:48:39 -0500

    add cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon()],
+  output: "server",
+  adapter: cloudflare()
 });
