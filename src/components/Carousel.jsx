8f387625d10e7b09f292790b7ece31a9d0b0e367
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2024-07-23 20:31:29 -0500

    fix carrousel data

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,30 +1,94 @@
-import React, { useEffect, useRef } from "react";
-import KeenSlider from "keen-slider";
+import React, { useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
+import "./styles.css";
 
 const Carousel = ({ slides }) => {
-  const sliderRef = useRef(null);
-
-  useEffect(() => {
-    const slider = new KeenSlider(sliderRef.current, {
-      loop: true,
-    });
-
-    return () => {
-      slider.destroy();
-    };
-  }, []);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
 
   return (
-    <div ref={sliderRef} className="keen-slider">
-      {slides.map((slide, idx) => (
-        <div key={idx} className="keen-slider__slide">
-          <DataGeneral {...slide} />
+    <>
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
+          {slides.map((slide, idx) => (
+            <div key={idx} className={`keen-slider__slide number-slide${idx}`}>
+              <DataGeneral {...slide} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+        {loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
+
+            <Arrow
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            />
+          </>
+        )}
+      </div>
+      {loaded && instanceRef.current && (
+        <div className="dots">
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx);
+                }}
+                className={"dot" + (currentSlide === idx ? " active" : "")}
+              ></button>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
+
+function Arrow(props) {
+  const disabled = props.disabled ? " arrow--disabled" : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? "arrow--left" : "arrow--right"
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  );
+}
 
 const DataGeneral = ({ title, description, img1, img2 }) => (
   <div className="p-4 mt-8">
