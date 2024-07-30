af4dd72b95a2a8e06b3670f00f65c0483f57f267
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2024-07-30 12:34:35 -0500

    [UPDATE] insight index content

--- src/components/GeneralDta.jsx
+++ src/components/GeneralDta.jsx
@@ -48,14 +48,14 @@ const GeneralDta = () => {
 
   const Feature = ({ name, description, nameIcon }) => {
     return (
-      <div key={name} className="relative pl-16">
+      <div key={name} className="relative pl-16  px-4 ">
         <dt className="text-base font-semibold leading-7 text-gray-900">
           <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-forest">
             {nameIcon}
           </div>
           {name}
         </dt>
-        <dd className="mt-2 text-base leading-7 text-gray-600 max-w-md">
+        <dd className="mt-2 text-base leading-7 text-gray-600 w-full ">
           {description}
         </dd>
         <a
@@ -69,15 +69,15 @@ const GeneralDta = () => {
   };
 
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-forest">
-            Grupo Hyperion
-          </h2>
+    <div className="bg-white py-24 sm:py-16">
+      <div className="mx-auto max-w-7xl  px-6 lg:px-8 xl:max-w-full">
+        <div className="mx-auto max-w-2xl lg:text-center lg:max-w-6xl">
           <p className="mt-2 text-3xl font-bold tracking-tight text-coffee sm:text-4xl">
             Reto: Biodiversidad
           </p>
+          <h2 className="text-base font-semibold leading-7 text-forest">
+            Grupo Hyperion
+          </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Nuestro reto es abordar la falta de información precisa y accesible
             sobre la{" "}
@@ -101,8 +101,8 @@ const GeneralDta = () => {
             </strong>
           </p>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+        <div className="mx-auto mt-16 md:w-full  sm:mt-20 lg:mt-24 lg:w-3/4">
+          <dl className="grid grid-cols-1 gap-x-8 gap-y-10 w-full max-w-none lg:grid-cols-2 lg:gap-y-16">
             {features.map((feature) => (
               <Feature key={feature.name} {...feature} />
             ))}
